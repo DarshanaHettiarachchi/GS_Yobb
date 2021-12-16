98e13fb6284b06decbbf73e9ef84e20ec6910a7b
Darshana Hettiarachchi <dnhnuwan@gmail.com>
2021-12-16 10:53:26 +0530

    Add styling to console statements.

--- src/q3-products/app.js
+++ src/q3-products/app.js
@@ -8,29 +8,3 @@ export const sortByPriceAscending = (productListJsonString) => {
 
   return JSON.stringify(sortedProductArray);
 };
-
-const runSorting = () => {
-  console.info(
-    "without same price - Before sort =>",
-    '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
-  );
-  console.info(
-    "sorted => ",
-    sortByPriceAscending(
-      '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
-    )
-  );
-
-  console.info(
-    "with same price - Before sort =>",
-    '[{"name":"eggs","price": 1},{"name":"apple","price":1},{"name":"rice","price":4.04}]'
-  );
-  console.info(
-    "sorted => ",
-    sortByPriceAscending(
-      '[{"name":"eggs","price": 1},{"name":"apple","price":1},{"name":"rice","price":4.04}]'
-    )
-  );
-};
-
-runSorting();
