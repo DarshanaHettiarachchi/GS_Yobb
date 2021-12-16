a3e321d41f6df4a60401b7364ef074fa0bf9e8c3
Darshana Hettiarachchi <dnhnuwan@gmail.com>
2021-12-16 10:53:59 +0530

    Move console logs to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-import "./q3-products/app";
+import { sortByPriceAscending } from "./q3-products/app";
+
+const runSorting = () => {
+  const beforeSortStyles =
+    "display: inline-block ; border: 3px solid red ; border-radius: 7px ; " +
+    "padding: 10px ; margin: 20px ;";
+
+  const sortedStyles =
+    "display: inline-block ; border: 3px solid green ; border-radius: 7px ; " +
+    "padding: 10px ; margin: 20px ;";
+
+  console.info(
+    "%cwithout same price - Before sort =>",
+    beforeSortStyles,
+    '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
+  );
+  console.info(
+    "%csorted => ",
+    sortedStyles,
+    sortByPriceAscending(
+      '[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'
+    )
+  );
+
+  console.info(
+    "%cwith same price - Before sort =>",
+    beforeSortStyles,
+    '[{"name":"eggs","price": 1},{"name":"apple","price":1},{"name":"rice","price":4.04}]'
+  );
+  console.info(
+    "%csorted => ",
+    sortedStyles,
+    sortByPriceAscending(
+      '[{"name":"eggs","price": 1},{"name":"apple","price":1},{"name":"rice","price":4.04}]'
+    )
+  );
+};
+
+runSorting();
